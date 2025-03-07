67b1d59b606c7527278339b5453fe91c4fd7f621
EwertonCaripuna <ewertoncaripuna@outlook.com>
2025-03-07 13:21:13 -0300

    feat: model Transaction

---
+++ src/main/java/com/web/desafioitaubackendnovo/DesafioItauBackendNovoApplication.java
@@ -0,0 +1,13 @@
+package com.web.desafioitaubackendnovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendNovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioItauBackendNovoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/desafioitaubackendnovo/DesafioItauBackendNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.desafioitaubackendnovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendNovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
